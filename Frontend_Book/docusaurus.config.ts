2044b72151029c7c54d99ea0193c57fed6895acd
shahrukh-khaliq <shahrukhkhaliq422@gmail.com>
2025-12-20 16:39:38 +0500

    update

--- Frontend_Book/docusaurus.config.ts
+++ Frontend_Book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://vercel.com/shahrukhs-projects-e0fad7ec/physical-ai-humanoid-textbook',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
